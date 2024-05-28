700b33aa7dcc221db2ada69879b5b58155eb7a16
macmini <nardis55five@gmail.com>
2024-05-28 14:22:17 -0500

    asdasdasdasdas

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import { retry } from "./utils/retry";
 import dotenv from "dotenv";
 import { ExtendedIDEXMarket } from "../src/init";
 import path from "path";
-import { setTimeout } from "timers/promises";
+import { setTimeout as sleep } from "timers/promises";
 import { generateOrderTemplate } from "./utils/generators";
 
 // import { db } from "./utils/mysqlConnector";
@@ -49,24 +49,24 @@ const main = async () => {
   }
 
   if (process.env.EXECUTE_ORDERS === "true") {
-    await execLoop(clients, initSide);
+    await execLoop(clients);
   }
 };
 
 async function fetchData(client: IClient, marketID: string): Promise<any> {
   return await Promise.all([
-    retry(() =>
-      client.RestAuthenticatedClient.getPositions({
-        market: marketID,
-        ...client.getWalletAndNonce,
-      })
-    ),
-    retry(() =>
-      client.RestAuthenticatedClient.getOrders({
-        ...client.getWalletAndNonce,
-        limit: Number(process.env.OPEN_ORDERS),
-      })
-    ),
+    // retry(() =>
+    //   client.RestAuthenticatedClient.getPositions({
+    //     market: marketID,
+    //     ...client.getWalletAndNonce,
+    //   })
+    // ),
+    // retry(() =>
+    //   client.RestAuthenticatedClient.getOrders({
+    //     ...client.getWalletAndNonce,
+    //     limit: Number(process.env.OPEN_ORDERS),
+    //   })
+    // ),
     retry(() =>
       client.RestPublicClient.getOrderBookLevel2({
         market: marketID,
@@ -76,174 +76,140 @@ async function fetchData(client: IClient, marketID: string): Promise<any> {
   ]);
 }
 
-async function execLoop(
-  clients: { [key: string]: IClient },
-  initSide: idex.OrderSide
-) {
-  let markets: ExtendedIDEXMarket[] = [];
-  let side: idex.OrderSide = initSide;
-  markets = await fetchMarkets();
-  // const marketsSubscription = markets.map(
-  //   (m) => `${m.baseAsset}-${m.quoteAsset}`
-  // );
+function adjustValueToResolution(value, resolution) {
+  let decimalsToKeep = 0;
 
-  // const handler = new WebSocketHandler(marketsSubscription, markets);
-  // await handler.initWebSocket();
+  // Determine the number of decimals to keep based on the resolution
+  switch (resolution) {
+    case "0.00000001":
+      decimalsToKeep = 8;
+      break;
+    case "0.00000010":
+      decimalsToKeep = 7;
+      break;
+    case "0.00000100":
+      decimalsToKeep = 6;
+      break;
+    case "0.00001000":
+      decimalsToKeep = 5;
+      break;
+    case "0.00010000":
+      decimalsToKeep = 4;
+      break;
+    case "0.00100000":
+      decimalsToKeep = 3;
+      break;
+    case "0.01000000":
+      decimalsToKeep = 2;
+      break;
+    case "0.10000000":
+      decimalsToKeep = 1;
+      break;
+    case "1.00000000":
+      decimalsToKeep = 0;
+      break;
+    case "10.00000000":
+      decimalsToKeep = -1;
+      break;
+    default:
+      throw new Error("Unsupported resolution format");
+  }
+
+  const factor = Math.pow(10, decimalsToKeep);
+  const adjustedValue = Math.floor(value * factor) / factor;
+
+  const val = adjustedValue.toFixed(Math.max(decimalsToKeep, 0));
+  return Number(val).toFixed(8);
+}
+
+async function execLoop(clients: { [key: string]: IClient }) {
+  let markets = await fetchMarkets();
+
+  const numberOfLevels = 10;
+  const stepPercentage = 0.00111;
 
   while (true) {
     try {
       for (const [accountKey, client] of Object.entries(clients)) {
-        let updatedMarkets = markets;
-        let cancelledOrders: boolean = false;
-        for (const market of updatedMarkets) {
+        for (const market of markets) {
           const marketID = `${market.baseAsset}-${market.quoteAsset}`;
-          let totalOrdersCount: number;
-          let currentOrderCount: number = 0;
           try {
-            let openPositions: idex.RestResponseGetPositions;
-            let openOrders: idex.RestResponseGetOrders;
-            let orderBook: idex.RestResponseGetOrderBookLevel2;
-            [openPositions, openOrders, orderBook] = await fetchData(
-              client,
-              marketID
-            );
+            let [orderBook] = await fetchData(client, marketID);
+            const indexPrice = parseFloat(orderBook.indexPrice);
+            const priceResolution = market.priceRes;
+            const quantityResolution = market.quantityRes;
 
-            market.indexPrice = orderBook.indexPrice;
+            for (let level = 0; level < numberOfLevels; level++) {
+              const priceIncrement = indexPrice * stepPercentage * (level + 1);
 
-            totalOrdersCount = +openOrders.length;
-            if (
-              totalOrdersCount >= Number(process.env.OPEN_ORDERS) &&
-              !cancelledOrders
-            ) {
-              ({ totalOrdersCount, cancelledOrders } = await CancelOrder(
-                client,
-                totalOrdersCount,
-                accountKey,
-                cancelledOrders,
-                marketID
-              ));
+              const buyPrice = adjustValueToResolution(
+                indexPrice - priceIncrement,
+                priceResolution
+              );
+              const sellPrice = adjustValueToResolution(
+                indexPrice + priceIncrement,
+                priceResolution
+              );
+
+              const buyParams = {
+                market: marketID,
+                type: "limit",
+                side: "buy",
+                price: buyPrice,
+                quantity: adjustValueToResolution(
+                  parseFloat(market.makerOrderMinimum) * 10,
+                  quantityResolution
+                ),
+              };
+
+              CreateOrder(client, { ...buyParams }, accountKey, marketID);
+
+              const sellParams = {
+                market: marketID,
+                type: "limit",
+                side: "sell",
+                price: sellPrice,
+                quantity: adjustValueToResolution(
+                  parseFloat(market.makerOrderMinimum) * 10,
+                  quantityResolution
+                ),
+              };
+              CreateOrder(client, { ...sellParams }, accountKey, marketID);
+              await sleep(500);
             }
-
-            let runMarket: boolean = true;
-            // @ts-ignore
-            ({ runMarket, side } = validateOrderSide(
-              orderBook,
-              side,
-              openPositions,
-              market
-            ));
-
-            const quantity = Number(market.makerOrderMinimum);
-
-            const orderParams = generateOrderTemplate(
-              Number(market.indexPrice),
-              quantity,
-              Number(market.takerOrderMinimum),
-              market.quantityRes,
-              market.priceRes,
-              market.iterations,
-              market.priceIncrement,
-              marketID,
-              side
-            );
-
-            const orderStartTime = Date.now();
-            logger.debug(JSON.stringify(orderParams, null, 2));
-            for (const orderParam of orderParams) {
-              if (
-                !runMarket &&
-                orderParam.type.toLowerCase().includes("market")
-              ) {
-                continue;
-              }
-              if (orderParam.quantity < Number(market.makerOrderMinimum)) {
-                orderParam.quantity = market.makerOrderMinimum;
-              }
-              if (
-                totalOrdersCount >= Number(process.env.OPEN_ORDERS) &&
-                !cancelledOrders
-              ) {
-                ({ totalOrdersCount, cancelledOrders } = await CancelOrder(
-                  client,
-                  totalOrdersCount,
-                  accountKey,
-                  cancelledOrders,
-                  marketID
-                ));
-                break;
-              } else {
-                totalOrdersCount++;
-                // logger.debug(JSON.stringify(orderParam, null, 2));
-                const order = await CreateOrder(
-                  client,
-                  orderParam,
-                  accountKey,
-                  marketID
-                ).then(() => {
-                  currentOrderCount++;
-                });
-                let sideIdentifier =
-                  side === idex.OrderSide.buy ? "BUY " : "SELL";
-
-                if (orderParam.type.includes("market")) {
-                  sideIdentifier = sideIdentifier === "BUY " ? "SELL" : "BUY ";
-                }
-
-                let price = orderParam.price || "market";
-                logger.debug(
-                  `${accountKey} ${marketID} ${sideIdentifier} order for at ${price}. ${totalOrdersCount}`
-                );
-
-                process.env.COOLDOWN === "true" &&
-                  (await setTimeout(
-                    Number(process.env.COOLDOWN_PER_ORDER) * 1000
-                  ));
-              }
-            }
-            const orderEndTime = Date.now();
-            const orderDuration = (orderEndTime - orderStartTime) / 1000;
-            const ordersPerSecond =
-              orderDuration > 0 ? currentOrderCount / orderDuration : 0;
             logger.info(
-              `OPS ${accountKey} ${marketID}: ${ordersPerSecond.toFixed(2)}`
+              `Processed loop for ${accountKey} on market ${marketID}`
             );
+            setTimeout(async () => {
+              logger.info(
+                `Timeout finish, cancelling orders for market ${marketID}`
+              );
+              try {
+                await client.RestAuthenticatedClient.cancelOrders({
+                  ...client.getWalletAndNonce,
+                  market: marketID,
+                });
+                logger.info(
+                  `Cancelled orders for ${accountKey} on market ${marketID}`
+                );
+              } catch (e) {
+                logger.error(
+                  `Error cancelling orders for ${accountKey} on market ${marketID}: ${e.message}`
+                );
+              }
+            }, 150000);
           } catch (e) {
             logger.error(
               `Error handling market operations for ${accountKey} on market ${marketID}: ${e.message}`
             );
-            await setTimeout(5000);
+            await sleep(5000);
           }
-
-          process.env.COOLDOWN === "true" &&
-            (await setTimeout(Number(process.env.COOLDOWN_PER_MARKET) * 1000));
-          side =
-            side === idex.OrderSide.buy
-              ? idex.OrderSide.sell
-              : idex.OrderSide.buy;
-          logger.info(
-            `Finished processing market ${marketID} for ${accountKey}.`
-          );
-          logger.info(
-            `Account ${accountKey} placed ${currentOrderCount} orders.`
-          );
+          await sleep(5000);
         }
-        const cooldownMessage =
-          process.env.COOLDOWN === "true"
-            ? `CD for ${process.env.COOLDOWN_PER_ACCOUNT}s`
-            : "";
-        logger.info(
-          `Finished processing markets for ${accountKey}. ${cooldownMessage}`
-        );
-        side =
-          side === idex.OrderSide.buy
-            ? idex.OrderSide.sell
-            : idex.OrderSide.buy;
-        process.env.COOLDOWN === "true" &&
-          (await setTimeout(Number(process.env.COOLDOWN_PER_ACCOUNT) * 1000));
       }
     } catch (e) {
       logger.error(`Error fetching markets: ${e.message}`);
-      await setTimeout(5000);
+      await sleep(5000);
       continue;
     }
   }
@@ -252,179 +218,6 @@ async function execLoop(
 main().catch((error) => {
   logger.error("Error during main function:", error);
 });
-
-function calculateBestPrice(bestAsk: any, bestBid: any, indexPrice: string) {
-  const parsedBestBid = Number(bestBid) > 0 ? Number(bestBid) : 0;
-  const parsedBestAsk = Number(bestAsk) > 0 ? Number(bestAsk) : 0;
-  const parsedIndexPrice = Number(indexPrice);
-
-  const bidWeight = Number(process.env.BEST_BID_WEIGHT) || 0.25;
-  const askWeight = Number(process.env.BEST_ASK_WEIGHT) || 0.25;
-  const indexWeight = Number(process.env.INDEX_PRICE_WEIGHT) || 0.5;
-
-  if (parsedBestBid === 0 && parsedBestAsk === 0) {
-    return parsedIndexPrice.toFixed(8);
-  }
-
-  let totalWeight = indexWeight;
-  let totalValue = indexWeight * parsedIndexPrice;
-
-  if (parsedBestBid > 0) {
-    totalWeight += bidWeight;
-    totalValue += bidWeight * parsedBestBid;
-  }
-
-  if (parsedBestAsk > 0) {
-    totalWeight += askWeight;
-    totalValue += askWeight * parsedBestAsk;
-  }
-
-  return (totalValue / totalWeight).toFixed(8);
-}
-
-function validateOrderSide(
-  orderBook: idex.RestResponseGetOrderBookLevel2,
-  side: string,
-  openPositions: any,
-  market: ExtendedIDEXMarket
-) {
-  const indexPrice = Number(market.indexPrice);
-
-  const weightFactorToIncludeInSideCalculation = 0.00111;
-  const weightFactorToIncludeInTotalValueCalculation = 0.00222;
-  const maxPriceDeviation = 0.001;
-
-  const bidsCalculation = calculateMarketMetrics(
-    orderBook.bids,
-    indexPrice,
-    weightFactorToIncludeInSideCalculation,
-    "weight"
-  );
-  const asksCalculation = calculateMarketMetrics(
-    orderBook.asks,
-    indexPrice,
-    weightFactorToIncludeInSideCalculation,
-    "weight"
-  );
-  const totalBidsValue = calculateMarketMetrics(
-    orderBook.bids,
-    indexPrice,
-    weightFactorToIncludeInTotalValueCalculation,
-    "totalValue"
-  ).totalValue;
-  const totalAsksValue = calculateMarketMetrics(
-    orderBook.asks,
-    indexPrice,
-    weightFactorToIncludeInTotalValueCalculation,
-    "totalValue"
-  ).totalValue;
-
-  const bidsWeight = bidsCalculation.weight * 0.98;
-  const asksWeight = asksCalculation.weight * 1.02;
-
-  if (bidsWeight > (bidsWeight + asksWeight) / 2) {
-    side = idex.OrderSide.sell;
-  } else {
-    side = idex.OrderSide.buy;
-  }
-
-  logger.debug(
-    `Bids Weight: ${bidsWeight}, Asks Weight: ${asksWeight}, Index Price: ${indexPrice}`
-  );
-  if (bidsCalculation.averagePrice > indexPrice) {
-    side = idex.OrderSide.sell;
-  } else if (asksCalculation.averagePrice < indexPrice) {
-    side = idex.OrderSide.buy;
-  }
-  logger.debug(
-    `Average Bid Price: ${bidsCalculation.averagePrice}, Average Ask Price: ${asksCalculation.averagePrice} within ${weightFactorToIncludeInSideCalculation} of index price ${indexPrice}`
-  );
-
-  let runMarket = true;
-
-  if (side === idex.OrderSide.sell && totalBidsValue < 10000) {
-    runMarket = false;
-  } else if (side === idex.OrderSide.buy && totalAsksValue < 10000) {
-    runMarket = false;
-  }
-
-  if (orderBook.asks.length < 20 || orderBook.bids.length < 20) {
-    if (
-      orderBook.asks.length < 20 &&
-      orderBook.asks.length < orderBook.bids.length
-    ) {
-      side = idex.OrderSide.sell;
-    } else if (
-      orderBook.bids.length < 20 &&
-      orderBook.bids.length < orderBook.asks.length
-    ) {
-      side = idex.OrderSide.buy;
-    }
-  }
-
-  if (
-    asksCalculation.averagePrice > indexPrice * (1 + maxPriceDeviation) ||
-    bidsCalculation.averagePrice < indexPrice * (1 - maxPriceDeviation)
-  ) {
-    side =
-      asksCalculation.averagePrice > indexPrice * (1 + maxPriceDeviation)
-        ? idex.OrderSide.sell
-        : idex.OrderSide.buy;
-  }
-
-  const bestBid =
-    orderBook.bids.length > 0 ? Number(orderBook.bids[0][0]) : null;
-  const bestAsk =
-    orderBook.asks.length > 0 ? Number(orderBook.asks[0][0]) : null;
-
-  if (bestAsk && bestAsk > indexPrice * (1 + maxPriceDeviation)) {
-    side = idex.OrderSide.sell;
-  } else if (bestBid && bestBid < indexPrice * (1 - maxPriceDeviation)) {
-    side = idex.OrderSide.buy;
-
-    logger.info(
-      `${
-        side === "buy"
-          ? `BUY  orders at ${market.indexPrice}, IP: ${indexPrice}`
-          : `SELL orders at ${market.indexPrice}, IP: ${indexPrice}`
-      }`
-    );
-  }
-  return { runMarket, side };
-}
-
-function calculateMarketMetrics(
-  orders: any,
-  indexPrice: number,
-  weightFactor: number,
-  mode: "weight" | "totalValue"
-) {
-  const filteredOrders = orders.filter(
-    ([price, _]: [string, string]) =>
-      Math.abs(Number(price) - indexPrice) / indexPrice <= weightFactor
-  );
-
-  if (mode === "weight") {
-    const totalWeight = filteredOrders.reduce(
-      (acc: any, [price, quantity]) => {
-        acc.total += Number(price) * Number(quantity);
-        acc.quantity += Number(quantity);
-        return acc;
-      },
-      { total: 0, quantity: 0 }
-    );
-    const averagePrice =
-      totalWeight.quantity > 0 ? totalWeight.total / totalWeight.quantity : 0;
-    return { weight: totalWeight.total, averagePrice };
-  } else {
-    // mode === 'totalValue'
-    const totalValue = filteredOrders.reduce(
-      (acc: any, [price, quantity]) => acc + Number(price) * Number(quantity),
-      0
-    );
-    return { totalValue };
-  }
-}
 
 async function CancelOrder(
   client: IClient,
@@ -452,7 +245,7 @@ async function CancelOrder(
           2
         )}`
       );
-      await setTimeout(1000);
+      await sleep(1000);
     });
   return { totalOrdersCount, cancelledOrders };
 }
@@ -478,6 +271,6 @@ async function CreateOrder(
       logger.error(`Trading disabled terminating process.`);
       process.exit();
     }
-    await setTimeout(1000);
+    await sleep(1000);
   });
 }
